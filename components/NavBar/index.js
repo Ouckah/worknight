c8da98c839b3f3e7c8cce5cabfa04628d0563957
Aidan <datbluemushroom@gmail.com>
2023-04-01 10:49:24 -0400

    💄 Navigation Bar

---
+++ components/NavBar/index.js
@@ -0,0 +1,16 @@
+const NavBar = () =>
+{
+    return (
+
+        <>
+        
+            <div className="w-full h-10 bg-white">
+                <h1 className="text-black">Navigation Bar!</h1>
+            </div>
+        
+        </>
+
+    )
+}
+
+export default NavBar;
