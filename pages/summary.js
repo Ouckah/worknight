b45649b669c82b6f6a2819ebc481c7ce6ebe11db
Milind <mckathiari@gmail.com>
2023-04-01 23:44:44 -0400

    label text needs fixing

--- pages/summary.js
+++ pages/summary.js
@@ -16,21 +16,23 @@ import MyCalendar from "../components/MyCalendar";
 import ClassesCardHolder from "../components/Classes Card Holder";
 
 export default function Summary() {
-    
+
     return (
         <>
             <div className="flex flex-row w-full h-screen bg-gray-100">
             <SideBar />
                 <div className='flex flex-row p-24'>
                     <div className = "flex flex-col">
-                    <MyCalendar />
-                            
+                        <MyCalendar />
+                        <div className="bg-gray-200 w-5/6 rounded-md">
+                            <label className="p-2 text-5xl underline font-mono">Graduation Status:</label>
+                            <label className= "p-4 text-2xl font-mono">You're on track to graduate according to schedule!</label>
+                        </div>
                     </div>
                     <ClassesCardHolder title="Degree Criteria" primary/>
                 </div> 
             </div>
         </>
-        
 
     )
 
