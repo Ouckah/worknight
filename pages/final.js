b572831f6b0593d732e889996887c082893761b8
Milind <mckathiari@gmail.com>
2023-04-01 23:14:50 -0400

    finish final WIP summary

--- pages/final.js
+++ pages/final.js
@@ -1,13 +1,46 @@
+import SideBar from "../components/SideBar";
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react'
+
+import { Button, ButtonGroup, Stack } from '@chakra-ui/react'
+
+import MyCalendar from "../components/MyCalendar";
+
 export default function Final() {
     
     return (
-
         <>
-        
-            
-        
+            <div className="flex flex-row w-full h-screen bg-gray-100">
+                <SideBar />
+                <div className="MAIN flex flex-col w-full h-full bg-gray-100 p-24 relative">
+                        <div className="w-full h-full">
+                            <MyCalendar />
+                        </div>
+                        <Stack spacing = {12} direction = 'row' align = 'center' position = 'relative'>
+                        <Button
+                            colorScheme='red'
+                            size = 'lg'>
+                        Deny
+                        </Button>
+                        <Button
+                            colorScheme='green'
+                            size = 'lg'>
+                        Accept
+                        </Button>
+                        </Stack>
+                </div>                        
+            </div>
         </>
-
+        
     )
 
 }
--- pages/summary.js
+++ pages/summary.js
@@ -1,12 +1,36 @@
+import SideBar from "../components/SideBar";
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react'
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import MyCalendar from "../components/MyCalendar";
+import ClassesCardHolder from "../components/Classes Card Holder";
+
 export default function Summary() {
     
     return (
-
         <>
-        
-            
-        
+            <div className="flex flex-row w-full h-screen bg-gray-100">
+            <SideBar />
+                <div className='flex flex-row p-24'>
+                    <div className = "flex flex-col">
+                    <MyCalendar />
+                            
+                    </div>
+                    <ClassesCardHolder title="Degree Criteria" primary/>
+                </div> 
+            </div>
         </>
+        
 
     )
 
